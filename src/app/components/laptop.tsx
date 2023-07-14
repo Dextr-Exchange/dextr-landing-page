78f318370940c8a851099df68db8c1dd76a32244
Kanishk Khurana <kanishkkhurana152@gmail.com>
2023-07-14 19:22:31 +0530

    responsiveness fixed

--- src/app/components/laptop.tsx
+++ src/app/components/laptop.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function laptop() {
   return (
-    <div className='flex py-12 xl:py-28 px-3 xl:px-20 max-h-fit flex-col xl:flex-row gap-8'>
-      <div className='xl:basis-2/3 h-full'>
-      <spline-viewer loading-anim url="https://prod.spline.design/trjcZQYHlScHX35r/scene.splinecode"></spline-viewer>  
+    <div className='flex py-12 xl:py-28 px-3 xl:px-20 max-h-fit iframe-cover flex-col xl:flex-row gap-8'>
+      <div className='xl:basis-2/3 h-full iframe-cover'>      
+      <spline-viewer hint className="iframe-cover" loading-anim url="https://prod.spline.design/trjcZQYHlScHX35r/scene.splinecode"></spline-viewer>  
       </div>
       <div className='flex justify-between flex-col xl:basis-1/3'>
         <h1 className='font-primary font-bold text-3xl xl:text-6xl text-center xl:text-right'>Always keep your assets with you, securely!</h1>
