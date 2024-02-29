9c36a7c49fd66faf85749b5a70aa456da8257a19
Kanishk Chhabra <67221487+mrkc2303@users.noreply.github.com>
2024-02-29 20:09:43 +0530

    Added minor changes

--- src/app/components/poweredBy.tsx
+++ src/app/components/poweredBy.tsx
@@ -6,12 +6,12 @@ import Biconomy from '../images/newBiconomy.png'
 
 export default function PoweredBy() {
   return (
-    <section className='w-[550px] mx-auto mt-10'>
-        <div className='font-primary border border-[#01C3F1] flex h-20 w-auto rounded-full mx-auto items-center justify-center shadow-inner'>
-            <span className='font-bold'>Powered By: </span>
-            <Image className='ml-3' src={ZetaChain} alt="ZetaChain" height={80} width={160} />
+    <section className='max-sm:w-[300px] w-[550px] mx-auto mt-10'>
+        <div className='font-primary max-sm:text-sm border border-[#01C3F1] flex h-20 w-auto rounded-full mx-auto items-center justify-center shadow-inner'>
+            <span className='font-bold max-sm:text-sm'>Powered By: </span>
+            <Image className='ml-3 max-sm:w-[80px] max-sm:ml-1' src={ZetaChain} alt="ZetaChain" height={80} width={160} />
             <span className='ml-3'> & </span>
-            <Image className='mx-3' src={Biconomy} alt="Biconomy" height={80} width={160} />
+            <Image className='mx-3 max-sm:w-[80px] max-sm:ml-1' src={Biconomy} alt="Biconomy" height={80} width={160} />
         </div>
     </section>
   )
