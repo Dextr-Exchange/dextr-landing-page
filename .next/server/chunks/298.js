589ecd20b6fdee7a2c7aa2e4ee460f62a49eabcc
Kanishk Chhabra <67221487+mrkc2303@users.noreply.github.com>
2024-09-18 17:43:00 -0500

    New Build

---
+++ .next/server/chunks/298.js
@@ -0,0 +1,337 @@
+exports.id = 298;
+exports.ids = [298];
+exports.modules = {
+
+/***/ 4580:
+/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
+
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 2927, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.bind(__webpack_require__, 9967));
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 8124, 23));
+Promise.resolve(/* import() eager */).then(__webpack_require__.t.bind(__webpack_require__, 209, 23))
+
+/***/ }),
+
+/***/ 3247:
+/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
+
+Promise.resolve(/* import() eager */).then(__webpack_require__.bind(__webpack_require__, 3858))
+
+/***/ }),
+
+/***/ 4386:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   Z: () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(6786);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var next_image__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(8421);
+/* harmony import */ var next_image__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(next_image__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _images_companylogo_svg__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(6935);
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(1621);
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(next_link__WEBPACK_IMPORTED_MODULE_3__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(8038);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_4__);
+/* harmony import */ var react_icons_hi__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(5655);
+/* harmony import */ var react_icons_ai__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(9722);
+/* harmony import */ var react_icons_fa__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(6775);
+/* harmony import */ var framer_motion__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(9591);
+
+
+
+
+
+
+
+
+
+const Header = ()=>{
+    const [toggleMenu, setToggleMenu] = (0,react__WEBPACK_IMPORTED_MODULE_4__.useState)(false);
+    return /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+        children: [
+            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)(framer_motion__WEBPACK_IMPORTED_MODULE_5__/* .motion */ .E.a, {
+                className: "bg-black text-gray-100 py-3 px-2 flex banner h-8 w-full justify-center items-center cursor-pointer font-semibold ease-in-out duration-500 hover:scale-105 max-sm:text-xs",
+                href: "https://twitter.com/Dextr_Exchange/status/1772980594705621318?t=Pw77powQ1MDvOLPo3lWnKA&s=19",
+                target: "_blank",
+                initial: {
+                    opacity: 0,
+                    y: -50
+                },
+                animate: {
+                    opacity: 1,
+                    y: 0
+                },
+                transition: {
+                    duration: 0.5
+                },
+                children: [
+                    "Applications Live \uD83D\uDD25 : Dextr Genius Guild!",
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("button", {
+                        className: "rounded-lg flex items-center gap-2 text-sm ml-5 bg-[#b12a29] p-2 max-sm:text-[8px]",
+                        children: [
+                            "Apply Now ",
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(react_icons_fa__WEBPACK_IMPORTED_MODULE_6__/* .FaArrowCircleRight */ .Wgy, {})
+                        ]
+                    })
+                ]
+            }),
+            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)(framer_motion__WEBPACK_IMPORTED_MODULE_5__/* .motion */ .E.header, {
+                className: "flex justify-between items-center py-6 px-12 max-sm:px-6",
+                initial: {
+                    opacity: 0,
+                    y: -50
+                },
+                animate: {
+                    opacity: 1,
+                    y: 0
+                },
+                transition: {
+                    duration: 1,
+                    delay: 0.5
+                },
+                children: [
+                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_link__WEBPACK_IMPORTED_MODULE_3___default()), {
+                        href: "/",
+                        className: "flex items-center space-x-2 max-sm:space-x-0",
+                        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_image__WEBPACK_IMPORTED_MODULE_1___default()), {
+                            src: _images_companylogo_svg__WEBPACK_IMPORTED_MODULE_2__/* ["default"] */ .Z,
+                            alt: "Dextr Logo",
+                            width: 150,
+                            height: 100
+                        })
+                    }),
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("nav", {
+                        className: "font-primary font-semibold text-gray-100 hidden lg:flex gap-12 justify-center items-center text-sm",
+                        children: [
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                                href: "#features",
+                                className: `hover:underline underline-offset-8 duration-150`,
+                                children: "FEATURES"
+                            }),
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                                href: "https://dextr-protocol.gitbook.io/dextr-mcamm-whitepaper-v3.0/",
+                                target: "_blank",
+                                rel: "noopener noreferrer",
+                                className: `hover:underline underline-offset-8 duration-150 `,
+                                children: " WHITEPAPER "
+                            }),
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                                href: "mailto:info@dextr.exchange",
+                                className: `hover:underline underline-offset-8 duration-150`,
+                                children: "CONTACT"
+                            })
+                        ]
+                    }),
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                        className: "flex relative mb-3",
+                        children: [
+                            !toggleMenu && /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(react_icons_hi__WEBPACK_IMPORTED_MODULE_7__/* .HiMenuAlt4 */ .iwI, {
+                                fontSize: 28,
+                                className: "text-white lg:hidden cursor-pointer h-full pt-3",
+                                onClick: ()=>setToggleMenu(true)
+                            }),
+                            toggleMenu && /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(react_icons_ai__WEBPACK_IMPORTED_MODULE_8__/* .AiOutlineClose */ .oHP, {
+                                fontSize: 28,
+                                className: "text-white hidden cursor-pointer",
+                                onClick: ()=>setToggleMenu(false)
+                            }),
+                            toggleMenu && /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("ul", {
+                                className: "z-10 fixed -top-0 -right-4 p-3 w-[60vw] h-full shadow-2xl lg:hidden gap-5 list-none flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in",
+                                children: [
+                                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("li", {
+                                        className: "text-xl w-full my-2 font-primary ",
+                                        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(react_icons_ai__WEBPACK_IMPORTED_MODULE_8__/* .AiOutlineClose */ .oHP, {
+                                            onClick: ()=>setToggleMenu(false)
+                                        })
+                                    }),
+                                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_link__WEBPACK_IMPORTED_MODULE_3___default()), {
+                                        href: "#features",
+                                        className: `hover:underline underline-offset-8 duration-150 mr-7`,
+                                        children: "FEATURES"
+                                    }),
+                                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                                        href: "https://dextr.gitbook.io/whitepaper/",
+                                        target: "_blank",
+                                        rel: "noopener noreferrer",
+                                        className: `hover:underline underline-offset-8 duration-150 mr-7`,
+                                        children: "WHITEPAPER"
+                                    }),
+                                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                                        href: "mailto:info@dextr.exchange",
+                                        className: `hover:underline underline-offset-8 duration-150 mr-7`,
+                                        children: "CONTACT"
+                                    }),
+                                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                                        href: "https://forms.zohopublic.com/brainchain/form/DextrBetaProgram1/formperma/jwTVjPwh8O2gVFb0TZSSjuBkc-IluEevhgOxTLzDqeY?utm_source=twitter&utm_medium=Zoho+SocialmG",
+                                        rel: "noopener noreferrer",
+                                        target: "_blank",
+                                        className: `font-primary font-semibold bg-gray-100 text-gray-800 px-4 py-2 rounded duration-150 mr-7`,
+                                        children: "App Coming Soon"
+                                    })
+                                ]
+                            })
+                        ]
+                    }),
+                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                        href: "https://forms.zohopublic.com/brainchain/form/DextrBetaProgram1/formperma/jwTVjPwh8O2gVFb0TZSSjuBkc-IluEevhgOxTLzDqeY?utm_source=twitter&utm_medium=Zoho+SocialmG",
+                        rel: "noopener noreferrer",
+                        target: "_blank",
+                        className: "hidden lg:block font-primary font-semibold bg-gray-100 text-gray-800 px-4 py-2 rounded",
+                        children: "App Coming Soon"
+                    })
+                ]
+            })
+        ]
+    });
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Header);
+
+
+/***/ }),
+
+/***/ 3858:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(6786);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(1621);
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(next_link__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _components_v2_header__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(4386);
+/* __next_internal_client_entry_do_not_use__ default auto */ 
+
+
+const Custom404 = ()=>{
+    return /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+        className: "min-h-screen flex flex-col overflow-x-hidden bg-radial-gradient",
+        children: [
+            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(_components_v2_header__WEBPACK_IMPORTED_MODULE_2__/* ["default"] */ .Z, {}),
+            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                className: "h-full mt-56 flex flex-col w-full justify-center items-center",
+                children: [
+                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("span", {
+                        className: "text-5xl text-white font-bold",
+                        children: "Opps.... Looks like you are lost"
+                    }),
+                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_link__WEBPACK_IMPORTED_MODULE_1___default()), {
+                        className: "text-white text-base mt-10",
+                        href: "/",
+                        children: "Let us help you get back to home page by clicking here"
+                    })
+                ]
+            })
+        ]
+    });
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Custom404);
+
+
+/***/ }),
+
+/***/ 6660:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (/* binding */ RootLayout),
+/* harmony export */   metadata: () => (/* binding */ metadata)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(6786);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _globals_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(5553);
+/* harmony import */ var _globals_css__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_globals_css__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var next_script__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(9663);
+/* harmony import */ var next_script__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(next_script__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var _next_third_parties_google__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(1084);
+
+
+
+
+const metadata = {
+    title: "Dextr Exchange",
+    description: "Tired of Low Returns and High Slippage? Discover Dextr: Maximize your earnings and minimize risk with Dextr's unique reputation-based AMM- take charge of your crypto journey today!"
+};
+function RootLayout({ children }) {
+    return /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("html", {
+        lang: "en",
+        children: [
+            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("head", {
+                children: [
+                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("link", {
+                        rel: "icon",
+                        type: "image/svg+xml",
+                        href: "/companylogo.svg"
+                    }),
+                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_script__WEBPACK_IMPORTED_MODULE_2___default()), {
+                        src: "https://cdn.pagesense.io/js/brainchain/f56e5f0b854a40d4a059d58ef7165b61.js"
+                    })
+                ]
+            }),
+            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("body", {
+                className: "scroll-smooth",
+                children: children
+            }),
+            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx(_next_third_parties_google__WEBPACK_IMPORTED_MODULE_3__/* .GoogleAnalytics */ .Wi, {
+                gaId: "G-H83B715197"
+            })
+        ]
+    });
+}
+
+
+/***/ }),
+
+/***/ 8708:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   $$typeof: () => (/* binding */ $$typeof),
+/* harmony export */   __esModule: () => (/* binding */ __esModule),
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var next_dist_build_webpack_loaders_next_flight_loader_module_proxy__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(1913);
+
+const proxy = (0,next_dist_build_webpack_loaders_next_flight_loader_module_proxy__WEBPACK_IMPORTED_MODULE_0__.createProxy)(String.raw`/Users/kanishkchhabra/dextr/dextr-landing-page/src/app/not-found.tsx`)
+
+// Accessing the __esModule property and exporting $$typeof are required here.
+// The __esModule getter forces the proxy target to create the default export
+// and the $$typeof value is for rendering logic to determine if the module
+// is a client boundary.
+const { __esModule, $$typeof } = proxy;
+const __default__ = proxy.default;
+
+
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__default__);
+
+/***/ }),
+
+/***/ 6935:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   Z: () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({"src":"/_next/static/media/companylogo.a466db2d.svg","height":54,"width":202,"blurWidth":0,"blurHeight":0});
+
+/***/ }),
+
+/***/ 5553:
+/***/ (() => {
+
+
+
+/***/ })
+
+};
+;
